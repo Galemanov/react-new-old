0c12479bef1baa381a75716eb8b2091576df8895
Alexander Galemanov <alexander.galemanov@gmail.com>
2022-09-30 18:46:11 +0300

    hotfix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/React">
+        <BrowserRouter basename="/index.html">
             <App />
         </BrowserRouter>
     </React.StrictMode>
